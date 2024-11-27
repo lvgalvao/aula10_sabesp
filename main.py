1ef5f3e33e8b9d5434a0697932036a4bdfa4c593
Luciano Filho <lvgalvaofilho@gmail.com>
2024-11-27 11:45:55 -0300

    fechei a task do logfire

--- main.py
+++ main.py
@@ -4,37 +4,43 @@ from typing import Dict
 import pandas as pd
 from time import sleep
 
+import logfire
+
+logfire.configure()
+
 URL = "https://api.coinbase.com/v2/prices/spot?currency=USD#"
 
 def extract(URL: str) -> Dict:
-    print("Começou o extract")
+    logfire.info("Começou o extract")
     response = requests.get(url=URL)
     data_dict = response.json()
     return data_dict
 
 def transform(data_dict: dict) -> DataFrame:
-    print("Começou a transformação")
+    logfire.info("Começou a transformação")
     data = data_dict
     dataframe = pd.DataFrame([data["data"]])
     dataframe["timestampt"] = pd.Timestamp.now()
     return dataframe
 
 def load(dataframe: DataFrame):
-    print("Começou o load")
+    logfire.info("Começou o load")
     dataframe.to_csv("coleta_bitcoin.csv", 
                      index=False, 
                      mode='a',
                      header=False
                      )
+    sleep(4)
+    logfire.info("Acabou o load")
     
 def pipeline(URL):
-    print("Começou a pipeline")
     data_dict_get = extract(URL)
     data_dataframe = transform(data_dict_get)
     load(data_dataframe)
-    print("Acabou a pipeline")
+    logfire.info("Acabou a pipeline")
 
 if __name__ == "__main__":
     while True:
-        pipeline(URL)
+        with logfire.span("Começando minha pipeline"):
+            pipeline(URL)
         sleep(5)
